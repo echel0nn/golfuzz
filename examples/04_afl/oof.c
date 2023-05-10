9a86b51260db3308224757f67e12fc78fad3b0b6
echel0n <melih.sahin@protonmail.com>
2023-05-10 03:32:54 +0300

    fixed custom mutator

--- examples/04_afl/oof.c
+++ examples/04_afl/oof.c
@@ -1,65 +1,79 @@
 /*
    AFL++ Custom Mutator for LibGolf
    Written by @echel0n
-   You need to use -I/path/to/AFLplusplus/include -I.
+   $ gcc -O3 -fPIC -shared -o golf_mutator.so -I ~/AFLplusplus/include/ oof.c 
  */
-#include "../../libgolf.h"
+#include "libgolf.h"
 #include "shellcode.h"
+#include "afl-fuzz.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define uchar unsigned char
+#define DATA_SIZE 0x100
 
-typedef struct post_state {
-  uchar *buf;
-  size_t size;
-} post_state_t;
+typedef struct my_mutator {
+  afl_state_t *afl;
+  // any additional data here!
+  size_t trim_size_current;
+  int trimmming_steps;
+  int cur_step;
+  u8 *mutated_out, *post_process_buf, *trim_buf;
+} my_mutator_t;
 
-void *afl_custom_init(void *afl) {
-  post_state_t *state = malloc(sizeof(post_state_t));
-  if (!state) {
-    perror("malloc");
+my_mutator_t *afl_custom_init(afl_state_t *afl, unsigned int seed) {
+  srand(seed); // needed also by surgical_havoc_mutate()
+  my_mutator_t *data = calloc(1, sizeof(my_mutator_t));
+  if (!data) {
+    perror("afl_custom_init alloc");
     return NULL;
   }
-
-  state->buf = calloc(sizeof(unsigned char), 4096);
-  if (!state->buf) {
-
-    free(state);
-    perror("calloc");
+  if ((data->mutated_out = (u8 *)malloc(MAX_FILE)) == NULL) {
+    perror("afl_custom_init malloc");
     return NULL;
   }
-
-  return state;
+  if ((data->post_process_buf = (u8 *)malloc(MAX_FILE)) == NULL) {
+    perror("afl_custom_init malloc");
+    return NULL;
+  }
+  if ((data->trim_buf = (u8 *)malloc(MAX_FILE)) == NULL) {
+    perror("afl_custom_init malloc");
+    return NULL;
+  }
+  data->afl = afl;
+  return data;
 }
 
-/*
- *
- * NEEDS TO BE FIXED: not producing ELFs that are loadable by Linux Loader
- */
-size_t afl_custom_post_process(post_state_t *data, unsigned char *in_buf,
-                               unsigned int len, unsigned char **out_buf) {
+size_t afl_custom_fuzz(my_mutator_t *data, uint8_t *in_buf, size_t buf_size,
+                       u8 **out_buf, uint8_t *add_buf,
+                       size_t add_buf_size, // add_buf can be NULL
+                       size_t max_size) {
 
-  if (len < 126)
-    return 0; // libgolf's smallest bin
+  RawBinary elf_obj;
+  RawBinary *elf = &elf_obj;
+  elf->isa = 62;
+  Elf64_Ehdr *ehdr;
+  Elf64_Phdr *phdr;
+  copy_text_segment(elf, buf, sizeof(buf));
+  ehdr = populate_ehdr(elf);
+  phdr = populate_phdr(elf);
+  set_entry_point(elf);
 
-  // need to keep the position
-  unsigned int pos = 0;
-  unsigned char *new_buf = malloc(sizeof(RawBinary));
-  INIT_ELF(X86_64, 64);
+  size_t mutated_size = ehdr_size + phdr_size + elf->text.text_size;
+  int pos = 0;
 
   ehdr->e_ident[EI_CLASS] = (uint8_t *)(in_buf + pos);
-  ++pos;
+  pos = pos + 1;
   ehdr->e_ident[EI_DATA] = (uint8_t *)(in_buf + pos);
-  ++pos;
+  pos = pos + 1;
   ehdr->e_ident[EI_VERSION] = (uint8_t *)(in_buf + pos);
-  ++pos;
+  pos = pos + 1;
   ehdr->e_ident[EI_OSABI] = (uint8_t *)(in_buf + pos);
-  ++pos;
-  for (int i = 0; i < 0x10; ++i) {
+  pos = pos + 1;
+  for (int i = 0x8; i < 0x10; ++i) {
     (ehdr->e_ident)[i] = (uint8_t *)(in_buf + pos);
-    ++pos;
+    pos = pos + 1;
   }
 
   ehdr->e_version = (uint32_t *)(in_buf + pos);
@@ -81,21 +95,23 @@ size_t afl_custom_post_process(post_state_t *data, unsigned char *in_buf,
   phdr->p_align = (uint64_t *)(in_buf + pos);
   pos = pos + 8;
 
-  // let's mimic GEN_ELF()
-  /*
+  /* let's mimic GEN_ELF()
    * Write:
    * - ELF Header
    * - Program Header
    * - Text Segment
    */
-  memcpy(new_buf, ehdr, ehdr_size);
-  memcpy(new_buf + ehdr_size, phdr, phdr_size);
-  memcpy(new_buf + ehdr_size + phdr_size, elf->text.text_segment,
+  memcpy(data->mutated_out, ehdr, ehdr_size);
+  memcpy(data->mutated_out + ehdr_size, phdr, phdr_size);
+  memcpy(data->mutated_out + ehdr_size + phdr_size, elf->text.text_segment,
          elf->text.text_size);
-  *out_buf = new_buf;
-  return ehdr_size + phdr_size + elf->text.text_size;
+
+  *out_buf = data->mutated_out;
+  return mutated_size;
 }
-void afl_custom_deinit(post_state_t *data) {
-  free(data->buf);
+void afl_custom_deinit(my_mutator_t *data) {
+  free(data->post_process_buf);
+  free(data->mutated_out);
+  free(data->trim_buf);
   free(data);
 }
